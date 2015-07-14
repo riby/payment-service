dcace668182691fe797705574251f7c5790cf98c
riby <ravneet.sdh@gmail.com>
2015-07-14 13:27:31 -0700

    ignore

---
+++ src/main/java/org/example/poc/dao/PaymentRepository.java
@@ -0,0 +1,12 @@
+package org.example.poc.dao;
+
+import org.example.poc.model.Payment;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by riby on 7/14/15.
+ */
+public interface PaymentRepository extends CrudRepository<Payment,Long>{
+
+
+}
